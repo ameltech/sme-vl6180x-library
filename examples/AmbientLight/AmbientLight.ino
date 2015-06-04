ea624fdcbff0021446759baaf43e5acfa3eaace0
MiK <smkk@amel-tech.com>
2015-06-04 21:10:34 +0200

    Added SerialUSB console in examples

--- examples/AmbientLight/AmbientLight.ino
+++ examples/AmbientLight/AmbientLight.ino
@@ -3,16 +3,16 @@
 
     Demonstrate how to detect the quantity of lux in a polling mode
 
-	And the ends the green led continuously blink.
+    And the ends the Blue led continuously blink.
 
     created 27 Apr 2015
     by Mik (smkk@amel-tech.com)
 
-	This example is in the public domain
-	https://github.com/ameltech
+    This example is in the public domain
+    https://github.com/ameltech
 
-	VL6180  more information available here:
-	http://www.st.com/web/catalog/mmc/FM132/SC626/PF260441?icmp=pf260441_pron_p3609p_sep2014&sc=proximitysensor
+    VL6180  more information available here:
+    http://www.st.com/web/catalog/mmc/FM132/SC626/PF260441?icmp=pf260441_pron_p3609p_sep2014&sc=proximitysensor
  */
 
 #include <Wire.h>
@@ -23,27 +23,27 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+    // initialize digital pin 13 as an output.
+    pinMode(PIN_LED_BLUE, OUTPUT);
 
-  smeAmbient.begin();
+    smeAmbient.begin();
 
-  SerialUSB.begin(115200);
+    SerialUSB.begin(115200);
 
 }
 
 // the loop function runs over and over again forever
 void loop() {
 
-  digitalWrite(13, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);             // wait for a second
+    digitalWrite(PIN_LED_BLUE, LOW);   // turn the LED ON by making the voltage LOW
+    delay(1000);             // wait for a second
 
-  float ligth = smeAmbient.ligthPollingRead();
+    float ligth = smeAmbient.ligthPollingRead();
 
-  printDouble(ligth, 100);
+    printDouble(ligth, 100);
 
-  digitalWrite(13, HIGH);   // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
+    digitalWrite(PIN_LED_BLUE, HIGH);   // turn the LED off by making the voltage HIGH
+    delay(1000);              // wait for a second
 
 }
 
@@ -51,23 +51,25 @@ void loop() {
 
 
 void printDouble( double val, unsigned int precision){
-// prints val with number of decimal places determine by precision
-// NOTE: precision is 1 followed by the number of zeros for the desired number of decimal places
-// example: printDouble( 3.1415, 100); // prints 3.14 (two decimal places)
+    // prints val with number of decimal places determine by precision
+    // NOTE: precision is 1 followed by the number of zeros for the desired number of decimal places
+    // example: printDouble( 3.1415, 100); // prints 3.14 (two decimal places)
 
-   SerialUSB.print (int(val));  //prints the int part
-   SerialUSB.print("."); // print the decimal point
-   unsigned int frac;
-   if(val >= 0)
-     frac = (val - int(val)) * precision;
-   else
-      frac = (int(val)- val ) * precision;
-   int frac1 = frac;
-   while( frac1 /= 10 )
-       precision /= 10;
-   precision /= 10;
-   while(  precision /= 10)
-       SerialUSB.print("0");
+    SerialUSB.print (int(val));  //prints the int part
+    SerialUSB.print("."); // print the decimal point
+    unsigned int frac;
+    if(val >= 0)
+        frac = (val - int(val)) * precision;
+    else
+        frac = (int(val)- val ) * precision;
+    int frac1 = frac;
+	
+    while( frac1 /= 10 )
+        precision /= 10;
+    precision /= 10;
+	
+    while(  precision /= 10)
+        SerialUSB.print("0");
 
-   SerialUSB.println(frac,DEC) ;
+    SerialUSB.println(frac,DEC) ;
 }
--- examples/Proximity/Proximity.ino
+++ examples/Proximity/Proximity.ino
@@ -1,18 +1,18 @@
 /*
     SmeProximity Library -Proximity
 
-    Demonstrate how to detect the proximity of an objet in polling mode.
-	
-	And the ends the green led continuously blink. 
+    Demonstrate how to detect the proximity of an object in polling mode.
+
+    And the ends the green led continuously blink. 
 
     created 27 Apr 2015
     by Mik (smkk@amel-tech.com)
 
-	This example is in the public domain
-	https://github.com/ameltech
-	 
-	VL6180  more information available here:	 
-	http://www.st.com/web/catalog/mmc/FM132/SC626/PF260441?icmp=pf260441_pron_p3609p_sep2014&sc=proximitysensor
+    This example is in the public domain
+    https://github.com/ameltech
+
+    VL6180  more information available here:     
+    http://www.st.com/web/catalog/mmc/FM132/SC626/PF260441?icmp=pf260441_pron_p3609p_sep2014&sc=proximitysensor
  */  
 
 #include <Wire.h>
@@ -24,26 +24,25 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
     // initialize digital pin 13 as an output.
-    pinMode(13, OUTPUT);
-	
-    smeProximity.begin();
-  
+    pinMode(PIN_LED_BLUE, OUTPUT);
 
-	Serial1.begin(115200);
-    
+    smeProximity.begin();
+
+    SerialUSB.begin(115200);
+
 }
 
 // the loop function runs over and over again forever
 void loop() {
+
+    digitalWrite(PIN_LED_BLUE, LOW);   // turn the LED off by making the voltage LOW
+    delay(1000);             // wait for a second
+
+    char ligth = smeProximity.rangePollingRead();    
+
+    SerialUSB.println(ligth, DEC);
 	
-	digitalWrite(13, LOW);   // turn the LED off by making the voltage LOW
-	delay(1000);             // wait for a second
-
-    float ligth = smeProximity.rangePollingRead();    
-
-	/*Serial1.write(ligth, DEC);*/
-
-    digitalWrite(13, HIGH);   // turn the LED off by making the voltage LOW
+    digitalWrite(PIN_LED_BLUE, HIGH);   // turn the LED off by making the voltage LOW
     delay(1000);              // wait for a second
 
 }
